74a47d3230a0cbba27f5438ad5d17c6d249a11a5
mahmoudgharib24 <67001964+mahmoudgharib24@users.noreply.github.com>
2020-06-18 11:35:16 +0200

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,3 +1,16 @@
+#type:
+#feat: Introduction
+#This Python script
+#subject:
+#Over the past decade, bicycle-sharing systems have been growing in number and popularity in cities across the world.
+#Bicycle-sharing systems allow users to rent bicycles on a very short-term basis for a price. This allows people to borrow a bike 
+#from point A and return it at point B, though they can also return it to the same location if they'd like to just go for a ride.
+#Regardless, each bike can serve several users per day.
+#Boddy: 
+#in this project, you will make use of Python to exploring data related to bike share systems for three major cities
+#in the United States—Chicago, New York City, and Washington. You will write code to import the data and answer interesting 
+#questions about it by computing descriptive statistics. You will also write a script that takes in raw input to create
+#an interactive experience in the terminal to present these statistics.
 import time
 import pandas as pd
 import numpy as np
